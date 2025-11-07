d67bfe29d0322eb5cfc2e0ecf0967f69aaaf9bd0
Lucas Carreño <lucascarreno559@gmail.com>
2025-11-07 13:27:08 -0300

    Contacto js listo

--- recursos/js/contacto.js
+++ recursos/js/contacto.js
@@ -1,13 +1,76 @@
 function ValidacionFormulario(event) {
-    // agregar codigo para funcionalidad del contacto y validaciones
-    // falta un div para agregar errores en el html o mostrarlos debajo de los inputs como te parezca mejor
-        
-    const formulario = document.getElementById("formulario-contacto");
-    const nombreInput = document.getElementById("nombre");
-    const emailInput = document.getElementById("email"); // agregale la validacion del .com si queres
-    const telefonoInput = document.getElementById("telefono");
-    const mensajeText = document.getElementById("mensaje");
-    event.preventDefault();
+  event.preventDefault();
+  let formValid = true;
+  // agregar codigo para funcionalidad del contacto y validaciones
+  // falta un div para agregar errores en el html o mostrarlos debajo de los inputs como te parezca mejor
 
+  /*======================================Obtencion de elementos===============================================*/
 
-};
+  const formulario = document.getElementById("formularioContacto");
+
+  const nombreInput = document.getElementById("nombre");
+  const errorNombreDiv = document.getElementById("errorNombre");
+  errorNombreDiv.textContent = "";
+
+  const emailInput = document.getElementById("email"); // agregale la validacion del .com si queres ----> Y si no quiero?
+  const errorEmailDiv = document.getElementById("errorEmail");
+  errorEmailDiv.textContent = "";
+
+  const telefonoInput = document.getElementById("telefono");
+  const errorTelefonoDiv = document.getElementById("errorTelefono");
+  errorTelefonoDiv.textContent = "";
+
+  const mensajeExitoDiv = document.getElementById("Exito-mensaje");
+  mensajeExitoDiv.textContent = "";
+  mensajeExitoDiv.classList.remove("Exito-mensaje");
+
+  /*======================================Validaciones===============================================*/
+
+  const nombreValor = nombreInput.value.trim();
+  const regexNombre = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+  if (regexNombre.test(nombreValor) === false) {
+    errorNombreDiv.textContent =
+      "El nombre solo debe contener letras y espacios.";
+    formValid = false;
+  } else if (nombreValor.length === 0) {
+    errorNombreDiv.textContent = "El campo nombre es obligatorio.";
+    formValid = false;
+  } else if (nombreValor.length < 5) {
+    errorNombreDiv.textContent = "El nombre debe tener al menos 5 caracteres.";
+    formValid = false;
+  }
+
+  const emailValor = emailInput.value.trim();
+  const regexEmail = /^[^\s@]+@[^\s@]+\.(com|ar|com\.ar)$/;
+  if (!emailValor) {
+    errorEmailDiv.textContent = "El campo email es obligatorio.";
+    formValid = false;
+  } else if (!regexEmail.test(emailValor)) {
+    errorEmailDiv.textContent =
+      "El formato de email no es valido (ingresar por ej. lucas50@gmail.com/.ar / hotmail.com/.ar).";
+    formValid = false;
+  }
+
+  const telefonoValor = telefonoInput.value.trim();
+  const regexTelefono = /^\d{7,15}$/;
+  if (!telefonoValor) {
+    errorTelefonoDiv.textContent = "El campo telefono es obligatorio.";
+    formValid = false;
+  } else if (!regexTelefono.test(telefonoValor)) {
+    errorTelefonoDiv.textContent =
+      "El formato de telefono no es valido (debe ser por ej. 2664786574).";
+    formValid = false;
+  }
+
+  /*======================================Mensaje de exito===============================================*/
+
+  if (formValid) {
+    mensajeExitoDiv.textContent = "¡Mensaje enviado con exito!";
+    mensajeExitoDiv.classList.add("Exito-mensaje");
+    formulario.reset();
+  } else {
+    mensajeExitoDiv.classList.remove("Exito-mensaje");
+    mensajeExitoDiv.textContent = "";
+  }
+  return false;
+}
--- recursos/js/myScript.js
+++ recursos/js/myScript.js
@@ -1,7 +1,5 @@
-
-document.addEventListener("DOMContentLoaded", function () { 
-    // agregar codigo para funcionalidad general esta incluido en el index , nosotros, productos y contacto
-    // buscar algo q sea comun en todas las paginas que ? nose?
-    
+document.addEventListener("DOMContentLoaded", function () {
+  // agregar codigo para funcionalidad general esta incluido en el index , nosotros, productos y contacto
+  // buscar algo q sea comun en todas las paginas que ? nose?
+  //Nose.... Segui pensando vos por que me tengo que ir a trabajarrr, ya deje listo la validacion del contacto :D
 });
-
